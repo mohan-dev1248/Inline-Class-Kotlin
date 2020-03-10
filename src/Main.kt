b3a8689c0a768e3f6fd146d77158c14fd63f552e
mohanakrishna <mohanakrishna96@gmail.com>
2020-03-10 13:48:55 +0530

    Writing methods and getters/setters to notice the bytecode generated

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,25 @@
 fun main(){
     val password = Password("Current Password")
+    val password2 = Password("Next Password")
 
     //Note that printing just this doesn't make much difference from the normal class
     println(password)
+    println(password2)
     println(password.password)
+
+    println(password.length)
+
+    password.sayThePasswordLength()
+    password2.sayThePasswordLength()
 }
 
-inline class Password(val password: String)
+inline class Password(val password: String){
+    val length: Int
+    get() = password.length// This is compiled to static method
+
+    //A static method
+    fun sayThePasswordLength(){
+        println("The password length is $length")
+    }
+}
 
