2c2bc026d72ab503b370e516eca0713dadcaecc9
mohanakrishna <mohanakrishna96@gmail.com>
2020-03-10 11:06:54 +0530

    Printing th inline class just like a normal class

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,9 @@
 fun main(){
-    println("Hello")
+    val password = Password("Current Password")
+
+    //Note that printing just this doesn't make much difference from the normal class
+    println(password)
+    println(password.password)
 }
 
 inline class Password(val password: String)
